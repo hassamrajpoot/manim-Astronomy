879f3a1effd06d27924b41077fc1c5a3624a7848
Hassam <hassamrajpoot100@gmail.com>
2024-10-01 02:27:10 +0500

    new changes

--- manim-Astronomy/main.py
+++ manim-Astronomy/main.py
@@ -1,39 +1,15 @@
-import numpy as np
 from manim import *
+from sympy import Plane
 from stellar_objects import *
-def wave_function(u, v, t):
-    wavelength = 0.5
-    amplitude = 0.2
-    distance_from_center = np.sqrt(u**2 + v**2)
-    return amplitude * np.sin(2 * np.pi * (distance_from_center - t) / wavelength)
-
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 class Space(ThreeDScene):
     def construct(self):
-        merged_masses = Dot3D(point=[0, 0, 0], radius=0.15, color=BLACK)
-        fabric = SpaceTimeFabric(
-            u_range=(-2, 2),
-            v_range=(-2, 2),
-            resolution=(50, 50),
-            t_range=(-2,2),
-            scaling_factor=0.1,
-        )
-        self.set_camera_orientation(phi=70 * DEGREES, theta=-90 * DEGREES)
-        self.add(fabric, merged_masses)
-        self.wait(1)
-        t_tracker = ValueTracker(0)
-
-        def update_fabric(mob):
-            t = t_tracker.get_value()
-
-            mob.become(SpaceTimeFabric(
-                u_range=(-2, 2),
-                v_range=(-2, 2),
-                resolution=(50, 50),
-                scaling_factor=0.1,
-                t_range=(-2,2),
-                func=wave_function,
-                func_args=(t,)
-            ))
-        fabric.add_updater(update_fabric)
-        self.play(t_tracker.animate.increment_value(5), run_time=10, rate_func=linear)
-        self.wait(2)
+        orbit1 = ThreeDVMobject().set_points_as_corners(Planet().compute_orbit())
+        self.add(orbit1.scale([2,0.5,1]))
+        orbit2 = ThreeDVMobject().set_points_as_corners(Planet(planet_name='Mars',mass=6.39e+23).compute_orbit(total_time=59354880))
+        self.add(orbit2.scale([2,0.5,1]))
+        orbit3 = ThreeDVMobject().set_points_as_corners(Planet(planet_name='Jupiter',mass=1.898e+27).compute_orbit(total_time=374335760))
+        self.add(orbit3.scale([2,0.5,1]))
+        self.move_camera(phi=70 * DEGREES, theta=-90 * DEGREES)
+        self.wait(3)
--- manim-Astronomy/stellar_objects.py
+++ manim-Astronomy/stellar_objects.py
@@ -2,6 +2,10 @@ from manim import *
 from manim.typing import Point3D
 import numpy as np
 from manim.mobject.opengl.opengl_compatibility import ConvertToOpenGL
+from astroquery.jplhorizons import Horizons
+from astropy import units as u
+import scipy
+import scipy.integrate
 
 class Star(ThreeDVMobject,metaclass=ConvertToOpenGL):
     SOLAR_PROPERTIES = {
@@ -91,6 +95,92 @@ class Star(ThreeDVMobject,metaclass=ConvertToOpenGL):
                 "mass" : self.mass
         }
     
+
+class Planet(ThreeDVMobject, metaclass=ConvertToOpenGL):
+    PLANETS = {
+        'Earth': 399,     
+        'Mars': 499,     
+        'Mercury': 199,   
+        'Venus': 299,     
+        'Saturn': 699,    
+        'Neptune': 899,   
+        'Uranus': 799,    
+        'Jupiter': 599   
+    }
+
+    def __init__(
+        self,
+        center=ORIGIN,
+        radius=0.05,  
+        planet_name='Earth',
+        initial_conditions=None,
+        mass=5.972e24,
+        **kwargs
+    ) -> None:
+        super().__init__(**kwargs)
+        
+        if planet_name not in self.PLANETS:
+            raise ValueError(f"Planet '{planet_name}' not found. Available planets: {list(self.PLANETS.keys())}")
+        
+        self.planet_name = planet_name
+        self.center = center
+        self.radius = radius
+        if initial_conditions is None:
+            initial_conditions = self.get_initial_conditions(self.planet_name)
+        self.position = initial_conditions["position"]
+        self.velocity = initial_conditions["velocity"]
+        self.mass = mass if mass is not None and mass != 0 else 1.0
+        self.planet_sphere = Dot3D(radius=self.radius, **kwargs).move_to(self.position)
+        self.add(self.planet_sphere)
+
+    def get_initial_conditions(self, planet_name):
+        planet_id = self.PLANETS[planet_name]
+        obj = Horizons(id=planet_id, location='@sun', epochs=2000.0)
+        eph = obj.vectors()
+        position = np.array([eph['x'][0], eph['y'][0], eph['z'][0]]) * 1.496e11
+        velocity = np.array([eph['vx'][0], eph['vy'][0], eph['vz'][0]])  * (1.496e11/86400)  
+        scale_factor_position = 1  
+        scale_factor_velocity = 1  
+        return {
+            "position": np.array(position) / scale_factor_position,
+            "velocity": np.array(velocity) / scale_factor_velocity
+        }
+
+    def compute_orbit(self, central_mass=1.989e30, dt=100000, total_time=31536000):
+        initial_conditions_data = self.get_initial_conditions(self.planet_name)
+        initial_conditions = [
+            initial_conditions_data['position'][0], initial_conditions_data['velocity'][0],
+            initial_conditions_data['position'][1], initial_conditions_data['velocity'][1],
+            initial_conditions_data['position'][2], initial_conditions_data['velocity'][2]
+                            ]
+
+        def f(t, state):
+            x, vx, y, vy, z, vz = state
+            r = np.sqrt(x**2 + y**2 + z**2) + 1e-5  
+            G = 6.67430e-11
+            Fx = -G * central_mass * self.mass * x / r**3
+            Fy = -G * central_mass * self.mass * y / r**3
+            Fz = -G * central_mass * self.mass * z / r**3
+            return [vx, Fx / self.mass, vy, Fy / self.mass, vz, Fz / self.mass]
+
+
+        t_span = (0, total_time)
+        t_eval = np.arange(0, total_time, dt)  
+        sol = scipy.integrate.solve_ivp(
+        f, 
+        t_span, 
+        initial_conditions, 
+        t_eval=t_eval, 
+        rtol=1e-3, 
+        atol=1e-6,
+        method='DOP853')
+        x = sol.y[0] / 1e12
+        y = sol.y[2] / 1e12
+        z = sol.y[4] / 1e12
+        positions = np.column_stack((x, y, z))
+        return positions
+
+
 
 class SpaceTimeFabric(ThreeDVMobject, metaclass=ConvertToOpenGL):
     def __init__(
